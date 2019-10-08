c37f0c5429859d71a1d4c0e3dd7b48513c436685
zhizit <sonoyama.engineer@gmail.com>
2019-10-08 18:05:15 +0900

    baseinit

---
+++ ApiBaseTraining/ApiBaseTraining/ViewController.swift
@@ -0,0 +1,15 @@
+//  ViewController.swift
+//  ApiBaseTraining
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ApiBaseTraining/README.md
@@ -0,0 +1,6 @@
+## storyboard を使わない実装
+
+- 以下のファイルと設定を削除
+  - デフォルトで生成されるViewController.swiftと、SceneDelegate.swift、storyboardファイルを全て削除
+  - Info.plist 「Main storyboard file base name」 を削除
+  - Info.plist 「Application Scene Manifest」 を削除
