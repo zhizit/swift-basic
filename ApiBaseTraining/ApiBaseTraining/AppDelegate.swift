f9e65a2ac764cd6e2d24fc0b8dc3005bd9da79c8
zhizit <sonoyama.engineer@gmail.com>
2019-10-09 10:02:02 +0900

    HOST切り替え

--- ApiBaseTraining/ApiBaseTraining/AppDelegate.swift
+++ ApiBaseTraining/ApiBaseTraining/AppDelegate.swift
@@ -6,27 +6,20 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    // UIを制御します
+    var window: UIWindow?
+    // Viewに表示するタイトルテキストです
+    var titleText: String = ""
+    // Viewに表示する詳細テキストです
+    var detailText: String = ""
+    // アプリを閉じるボタンを利用するかの判定です
+    var isEnableAppCloseBtn: Bool = true
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let host = Bundle.main.object(forInfoDictionaryKey: "customHost") as! String
+        print(host)
+
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
--- ApiBaseTraining/README.md
+++ ApiBaseTraining/README.md
@@ -2,5 +2,22 @@
 
 - 以下のファイルと設定を削除
   - デフォルトで生成されるViewController.swiftと、SceneDelegate.swift、storyboardファイルを全て削除
-  - Info.plist 「Main storyboard file base name」 を削除
-  - Info.plist 「Application Scene Manifest」 を削除
+  - Info.plist 「Main storyboard file base name」の「-」ボタンを押下して削除
+  - Info.plist 「Application Scene Manifest」の「-」ボタンを押下してを削除
+
+## APIのHOSTは環境によって切り替える（今回は、Info.plist をまとめて1つで管理するものとします）
+
+  - TARGETSを設定する
+    - 水色アイコンのプロジェクトを開き、TARGETSを選択
+    - TARGET名を「DEV_[プロジェクト名]」にしておく
+    - Combined Labels の横のプラスボタンから「Add User-Defined」を選択
+    - 「CUSTOM_HOST」のように、任意の変数名で、設定したい環境のドメイン名を設定する
+    - 右クリックしてDuplicateを選択し、後は増やしたい環境分だけ同じように設定していく
+  - Info.plist を設定する
+    - Info.plist > Information Property List 配下に、key：[好きな変数名] value:$(CUSTOM_HOST) を設定する
+  - HOSTを呼び出す処理を記載する
+    - ```let host = Bundle.main.object(forInfoDictionaryKey: "[前手順で設定した好きな変数名]") as! String```
+  - Xcode の左上のアイコンから Schemes を環境別に作成する
+    - 「Manage Schemes ...」を選択
+    - 「＋」ボタンを押下すると TARGET を指定できるので、環境分だけ　Schemes を作成する
+    - 後は確認したいSchemeを選択すれば環境が切り替わります
